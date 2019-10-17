ef00acfc2f3d5d77e35e0f609012e78caa7e3b56
MrVerdier <ande5630@stud.kea.dk>
2019-10-17 11:47:26 +0200

    Updates

---
+++ precache-manifest.24715191f041a7fbd655869ea2864962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537ae5bacd0d06f00accd113e011c2f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "256f9d16eef2bccda732",
+    "url": "/static/css/main.f7593fd7.chunk.css"
+  },
+  {
+    "revision": "bc15f6a4917a64400fa4",
+    "url": "/static/js/2.f8d7d83b.chunk.js"
+  },
+  {
+    "revision": "256f9d16eef2bccda732",
+    "url": "/static/js/main.7151d2c1.chunk.js"
+  },
+  {
+    "revision": "f85b3131cb64cd9819ba",
+    "url": "/static/js/runtime-main.06ddb292.js"
+  }
+]);
